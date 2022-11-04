01b98256a8ab86cd1940d5ee528ffde7ca2dfbd7
mbell9 <81600292+mbell9@users.noreply.github.com>
2022-11-04 17:29:08 -0400

    Cleaned Up
    Commented and deleted unused code

--- plotting.js
+++ plotting.js
@@ -1,19 +1,22 @@
-// import { randomNumber, linkCrossingsParam } from "./crossings.js";
+var array_of_edges = []; // trying to use global variable, idk
 
-var array_of_edges = [];
-
-function parseData() {
+// read data in the input box and generate random coordinates for each node
+function parseData() { 
 	var text = document.getElementById("dataset").value;
 	var lines = text.split("\n");
 	var edges = []
 	var nodes = {} 
+	
+	// skip header, read line-by-line
 	for(var i=1; i<lines.length-1; i++)
 	{
 		var l = lines[i]
 		var arr = l.split("   ")
-		var v1 = arr[0]
-		var v2 = arr[1]
-		if (!(v1 in nodes)) {
+		var v1 = arr[0] // vertex 1
+		var v2 = arr[1] // vertex 2
+		
+		// if the node is not assigned coordinates, generate them and add to nodes dict
+		if (!(v1 in nodes)) { 
 			var x1 = randomNumber(0, lines.length)
 			var y1 = randomNumber(0, lines.length)
 			nodes[v1] = {x:x1, y:y1}
@@ -29,24 +32,26 @@ function parseData() {
 			var x2 = nodes[v2].x
 			var y2 = nodes[v2].y
 		}
+		// object representing a line, (x1,y1) ---> (x2,y2)
 		edges.push({"x1":parseInt(x1), "x2":parseInt(x2), "y1":parseInt(y1), "y2":parseInt(y2)})
 	}
-	array_of_edges = [...edges];
+	array_of_edges = [...edges]; // want to store lines in a global variable, not sure how
 	return edges;
 }
 
+// should return an array of intersections, takes
 function findIntersection() {
-	console.log(array_of_edges)
-	const intersections = linkCrossingsParam(array_of_edges);
-	console.log(intersections)
+	console.log(array_of_edges) // TEST: prints empty
+	const intersections = linkCrossingsParam(array_of_edges); // method from crossings.js, on line 789
 	var textarea = document.getElementById("output");
 	textarea.value = intersections;
 }
 
+// take list of line objects {x1:x1,y1:y1,x2:x2,y2:y2}, draw lines in svg
 function drawLines(incomingData) {
 	var maxCoord = d3.max(incomingData, d => parseInt(d.length))
-	var xScale = d3.scaleLinear().domain([0,20]).range([0,580]);
-	var yScale = d3.scaleLinear().domain([0,20]).range([0,460]);
+	var xScale = d3.scaleLinear().domain([0,20]).range([0,580]); // x scale
+	var yScale = d3.scaleLinear().domain([0,20]).range([0,460]); // y scale
 	d3.select("svg").attr("style","height: 480px; width: 600px;");
 	d3.select("svg")
 	.selectAll("line")
@@ -58,43 +63,4 @@ function drawLines(incomingData) {
 	.attr("x2", d => xScale(parseInt(d.x2)))
 	.attr("y2", d => yScale(parseInt(d.y2)))
 	.style("stroke", "black")
-}
-
-function dataViz(incomingData) {
-	d3.selectAll("svg").selectAll("*").remove();
-
-	if (document.getElementById("bar").checked) {
-		var maxPopulation = d3.max(incomingData, d => parseInt(d.population))
-		var yScale = d3.scaleLinear().domain([0,maxPopulation]).range([0,460]);
-		d3.select("svg").attr("style","height: 480px; width: 600px;");
-		d3.select("svg")
-		.selectAll("rect")
-		.data(incomingData)
-		.enter()
-		.append("rect")
-		.attr("width", 50)
-		.attr("height", d => yScale(parseInt(d.population)))
-		.attr("x", (d,i) => i * 60)
-		.attr("y", d => 480 - yScale(parseInt(d.population)))
-		.style("fill", "#FE9922")
-		.style("stroke", "#9A8B7A")
-		.style("stroke-width", "1px")
-	}
-	else if (document.getElementById("scatter").checked) {
-		// var maxPopulation = d3.max(incomingData, d => parseInt(d.population))
-		var yScale = d3.scaleLinear().domain([0,100]).range([50,400]);
-		var radiusScale = d3.scaleLinear().domain([0,100]).range([5,30]);
-		d3.select("svg").attr("style","height: 480px; width: 600px;");
-		d3.select("svg")
-		.selectAll("circle")
-		.data(incomingData)
-		.enter()
-		.append("circle")
-		.attr("r", d => radiusScale(d))
-		.attr("cx", (d,i) => (i * 60) + 10)
-		.attr("cy", d => 480 - yScale(d))
-		.style("fill", "blue")
-		.style("stroke", "black")
-		.style("stroke-width", "1px");
-	}
 }
