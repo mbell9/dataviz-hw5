114b71359083f1aedc25879e0470a0376e583319
mbell9 <81600292+mbell9@users.noreply.github.com>
2022-11-07 16:44:06 -0500

    finished printIntersections() and cleaned up code
    - finished printIntersections() and cleaned up code
    - want to change svg dimensions to grow to size of webpage/graph

--- plotting.js
+++ plotting.js
@@ -1,4 +1,4 @@
-var array_of_edges = []; // trying to use global variable, idk
+var array_of_edges = []; // global variable to hold edges
 
 // read data in the input box and generate random coordinates for each node
 function parseData() { 
@@ -20,7 +20,7 @@ function parseData() {
 			var x1 = randomNumber(0, lines.length)
 			var y1 = randomNumber(0, lines.length)
 			nodes[v1] = {x:x1, y:y1}
-		} else {
+		} else { // if it exists, just store the line to be added to edges[]
 			var x1 = nodes[v1].x
 			var y1 = nodes[v1].y
 		}
@@ -32,35 +32,55 @@ function parseData() {
 			var x2 = nodes[v2].x
 			var y2 = nodes[v2].y
 		}
-		// object representing a line, (x1,y1) ---> (x2,y2)
-		edges.push({"x1":parseInt(x1), "x2":parseInt(x2), "y1":parseInt(y1), "y2":parseInt(y2)})
+
+		// object representing a line, source:(x,y) ---> target:(x,y)
+		edges.push({
+			source: {
+				x:parseInt(x1),
+				y:parseInt(y1), 
+				index:parseInt(v1) // node index
+			},
+			target: {
+				x:parseInt(x2),
+				y:parseInt(y2),
+				index:parseInt(v2)
+			},
+			index:i-1 // line segment index (first is line 0, second is line 1,...)	
+		});
 	}
-	array_of_edges = [...edges]; // want to store lines in a global variable, not sure how
+	array_of_edges = [...edges]; // store lines in a global variable
 	return edges;
 }
 
-// should return an array of intersections, takes
-function findIntersection() {
-	console.log(array_of_edges) // TEST: prints empty
-	const intersections = linkCrossingsParam(array_of_edges); // method from crossings.js, on line 789
+// finds intersections which lines intersect and outputs them
+function printIntersections() {
+	var intersections = linkCrossingsParam(array_of_edges);
 	var textarea = document.getElementById("output");
-	textarea.value = intersections;
+	textarea.value = "";
+
+	for (var i = 0; i < intersections.length; i+=2) {
+		textarea.value += "segment "+intersections[i][0][2]+" crosses segment "+intersections[i][1][2]+", ";
+	}
+	textarea.value = textarea.value.replace(/, $/, "") + "."; // replace trailing comma with period
 }
 
-// take list of line objects {x1:x1,y1:y1,x2:x2,y2:y2}, draw lines in svg
+// take list of line objects, draw lines in svg
 function drawLines(incomingData) {
-	var maxCoord = d3.max(incomingData, d => parseInt(d.length))
-	var xScale = d3.scaleLinear().domain([0,20]).range([0,580]); // x scale
-	var yScale = d3.scaleLinear().domain([0,20]).range([0,460]); // y scale
+
+	d3.selectAll("svg").selectAll("*").remove(); // clear previous drawing
+	var maxX = d3.max(incomingData, d => parseInt(d.source.x || d.target.x)) // get largest x coordinate
+	var maxY = d3.max(incomingData, d => parseInt(d.source.y || d.source.y)) // get largest y coordinate
+	var xScale = d3.scaleLinear().domain([0,maxX]).range([0,580]); // x scale
+	var yScale = d3.scaleLinear().domain([0,maxY]).range([0,460]); // y scale
 	d3.select("svg").attr("style","height: 480px; width: 600px;");
 	d3.select("svg")
 	.selectAll("line")
 	.data(incomingData)
 	.enter()
 	.insert("line")
-	.attr("x1", d => xScale(parseInt(d.x1)))
-	.attr("y1", d => yScale(parseInt(d.y1)))
-	.attr("x2", d => xScale(parseInt(d.x2)))
-	.attr("y2", d => yScale(parseInt(d.y2)))
+	.attr("x1", d => xScale(parseInt(d.source.x)))
+	.attr("y1", d => yScale(parseInt(d.source.y)))
+	.attr("x2", d => xScale(parseInt(d.target.x)))
+	.attr("y2", d => yScale(parseInt(d.target.y)))
 	.style("stroke", "black")
 }
